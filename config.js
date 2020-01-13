a236d1b3f0e20e3f42c719e55f4cfd2d74723b62
Pratheek Hegde <ptk609@gmail.com>
2020-01-13 13:06:49 +0530

    test netlify deploy

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://iam.pratheekheg.de',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: 'Pratheek Hegde',
+  subtitle: 'Javascript & Go Developer.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
@@ -25,14 +25,14 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Pratheek Hegde',
     photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'Full stack engineer from Bangalore.',
     contacts: {
       email: '',
       facebook: '#',
       telegram: '#',
-      twitter: '#',
+      twitter: '@pratheekhegde',
       github: '#',
       rss: '',
       vkontakte: '',
