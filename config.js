93117730962d52cbaa9550265534db3c63709a82
Pratheek Hegde <pratheekhegde@MMT-MAC-8224N.local>
2020-12-08 23:26:41 +0530

    fix bio typo

--- config.js
+++ config.js
@@ -1,49 +1,50 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://iam.pratheekheg.de',
-  pathPrefix: '/',
-  title: 'Pratheek Hegde - Web Engineer',
-  subtitle: 'Personal blog by Pratheek Hegde. Half/Full baked thoughts scribbled here',
-  copyright: '© for the free and open web.',
-  disqusShortname: '',
+  url: "https://iam.pratheekheg.de",
+  pathPrefix: "/",
+  title: "Pratheek Hegde - Web Engineer",
+  subtitle:
+    "Personal blog by Pratheek Hegde. Half/Full baked thoughts scribbled here",
+  copyright: "© for the free and open web.",
+  disqusShortname: "",
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-67738250-1',
+  googleAnalyticsId: "UA-67738250-1",
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: "Articles",
+      path: "/",
     },
     {
-      label: 'About me',
-      path: '/pages/about'
+      label: "About me",
+      path: "/pages/about",
     },
     {
-      label: 'My Impossible List',
-      path: '/pages/my-impossible-list'
-    }
+      label: "My Impossible List",
+      path: "/pages/my-impossible-list",
+    },
   ],
   author: {
-    name: 'Pratheek Hegde',
-    photo: '/white.png',
-    bio: 'Senior Software Enginner @ MakeMyTrip.',
+    name: "Pratheek Hegde",
+    photo: "/white.png",
+    bio: "Senior Software Engineer @ MakeMyTrip.",
     contacts: {
-      email: '',
-      facebook: 'pratheek.hegde',
-      telegram: 'pratheekhegde',
-      twitter: '@pratheekhegde',
-      github: 'pratheekhegde',
-      rss: '',
-      vkontakte: '',
-      linkedin: 'pratheekhegde',
-      instagram: 'pratheek_hegde',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: '',
-      soundcloud: '',
-    }
-  }
+      email: "",
+      facebook: "pratheek.hegde",
+      telegram: "pratheekhegde",
+      twitter: "@pratheekhegde",
+      github: "pratheekhegde",
+      rss: "",
+      vkontakte: "",
+      linkedin: "pratheekhegde",
+      instagram: "pratheek_hegde",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: "",
+    },
+  },
 };
